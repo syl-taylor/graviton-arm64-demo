4d3591654b5dce18eef9b2880a103f516ca5606c
Syl Taylor <44095554+syl-taylor@users.noreply.github.com>
2024-04-18 15:04:49 +0100

    Update app.py

--- codecommit/software_running/app.py
+++ codecommit/software_running/app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 rng = np.random.RandomState(1)
 X = np.sort(5 * rng.rand(100, 1), axis=0)
 y = np.sin(X).ravel()
-y[::5] += 3 * (0.5 - rng.rand(32))
+y[::5] += 3 * (0.5 - rng.rand(20))
 
 # Fit regression model
 dtreg_1 = DecisionTreeRegressor(max_depth=2)
@@ -16,8 +16,8 @@ dtreg_2.fit(X, y)
 
 # Predict
 X_test = np.arange(0.0, 5.0, 0.01)[:, np.newaxis]
-y_1 = regr_1.predict(X_test)
-y_2 = regr_2.predict(X_test)
+y_1 = dtreg_1.predict(X_test)
+y_2 = dtreg_2.predict(X_test)
 
 # Plot results
 plt.figure()
